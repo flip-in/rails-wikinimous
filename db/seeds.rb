2875174cfe3f5161a1c45bddd7441dd20bb850a1
Will Price <pricew86@gmail.com>
2020-02-25 17:49:52 +0800

    basic CRUD finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  title = Faker::Hipster.sentence
+  content = Faker::Hipster.paragraph
+  article = Article.new(title: title, content: content)
+  article.save
+end
